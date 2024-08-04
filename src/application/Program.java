5a8aac37d211e367dbbcacb57b785b879c0ca5e5
acmors <marcosantoniosilva047@gmail.com>
2024-08-04 16:59:51 -0300

    alguns ajustes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 	public static void main(String arg[]) {
-		Position p = new Position(3, 5);
-		System.out.println(p);
+		Board b = new Board(8, 8);
 	}
 }
